22abd3a9593f2c3a06ae5df3fc4f4f13333bba59
Ellery Zhang <allotory@msn.com>
2017-03-13 14:55:20 +0800

    feat: add main page

--- imclient/main.js
+++ imclient/main.js
@@ -13,17 +13,17 @@ let win
 
 function createWindow () {
     // 创建浏览器窗口。
-    win = new BrowserWindow({width: 430, height: 330, frame: false, resizable: false});
+    win = new BrowserWindow({width: 280, height: 680, frame: false, resizable: false});
 
     // 加载应用的 index.html。
     win.loadURL(url.format({
-        pathname: path.join(__dirname, '/html/login.html'),
+        pathname: path.join(__dirname, '/html/main.html'),
         protocol: 'file:',
         slashes: true
     }))
 
     // 打开开发者工具。
-    win.webContents.openDevTools();
+    // win.webContents.openDevTools();
 
     // 当 window 被关闭，这个事件会被触发。
     win.on('closed', () => {
